ec43d3bfa673c0469c5fad457a37e2071795ba7a
sohyeonjin <shj41738866@gmail.com>
2022-03-27 09:19:56 +0900

    add navigator

--- App.js
+++ App.js
@@ -8,14 +8,17 @@
 
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import BoxOffice from './pages/BoxOffice';
+
+const Stack = createNativeStackNavigator();
 
 const App: () => Node = () => {
   return (
     <>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="BoxOffice" component={BoxOffice } />
+          <Stack.Screen name="BoxOffice" component={BoxOffice} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
